f2f841b54b9b230c7affd3df301ab2028ec52b3f
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2021-06-06 08:04:56 +0000

    Initial setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+from hwaddress import __doc__, __version__
+
+project_directory = os.path.abspath(os.path.dirname(__file__))
+readme_path = os.path.join(project_directory, 'README.rst')
+
+readme_file = open(readme_path)
+try:
+    long_description = readme_file.read()
+finally:
+    readme_file.close()
+
+setup(
+    name='hwaddress',
+    version=__version__,
+    description=__doc__.split('\n')[0],
+    long_description=long_description,
+    license='0BSD',
+    url='https://github.com/mentalisttraceur/python-hwaddress',
+    author='Alexander Kozhevnikov',
+    author_email='mentalisttraceur@gmail.com',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    py_modules=['hwaddress'],
+)
